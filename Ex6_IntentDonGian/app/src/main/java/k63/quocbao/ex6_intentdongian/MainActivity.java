a81a6fca8c52d888f1ed9884ef464bf56af76deb
NguyenQuocBao63 <bao.nq.63cntt@ntu.edu.vn>
2025-03-12 15:04:46 +0700

    update

--- Ex6_IntentDonGian/app/src/main/java/k63/quocbao/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/k63/quocbao/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package k63.quocbao.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //tim dieu khien nut bam
+        TimDieuKhien();
+        // Gan bo lang nghe su kien
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xu ly chuyen man hinh
+                //B1 tao mot Intent 2 tham so: 1) man hinh hien tai 2) man hinh chuyen toi class
+                Intent intentMH2 = new Intent(MainActivity.this,MH2Activity.class);
+                //B2 Gui
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xu ly chuyen man hinh
+                //B1 tao mot Intent 2 tham so: 1) man hinh hien tai 2) man hinh chuyen toi class
+                Intent intentMH3 = new Intent(MainActivity.this,MH2Activity.class);
+                //B2 Gui
+                startActivity(intentMH3);
+            }
         });
     }
 }
