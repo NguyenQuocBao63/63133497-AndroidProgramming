92d33588576b90c1e3f4884645cdb441f25e4059
NguyenQuocBao63 <bao.nq.63cntt@ntu.edu.vn>
2025-03-26 14:03:00 +0700

    demo

--- UsingRecyclerView/app/src/main/java/k63/quocbao/usingrecyclerview/LandScapeAdapter.java
+++ UsingRecyclerView/app/src/main/java/k63/quocbao/usingrecyclerview/LandScapeAdapter.java
@@ -1,5 +1,7 @@
 package k63.quocbao.usingrecyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,23 +10,44 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+import java.util.ArrayList;
 
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
 
     @NonNull
     @Override
     public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land,parent,false);
+        ItemLandHolder viewholderCreated = new ItemLandHolder(vItem);
+        return  viewholderCreated;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
-
+        // Lay doi tuong hien thi
+        LandScape landScapeHienThi = lstData.get(position);
+        //Trich thong tin
+        String caption = landScapeHienThi.getLandCaption();
+        String tenFileAnh = landScapeHienThi.getLandImageFileName();
+        // Dat vao cac truong thong tin cua holder
+        holder.tvCaption.setText(caption);
+        // dat anh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh,"mipmap",packageName);
+        holder.ivLandScape.setImageResource(imageID);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstData.size();
     }
 
     class ItemLandHolder extends RecyclerView.ViewHolder{
--- UsingRecyclerView/app/src/main/java/k63/quocbao/usingrecyclerview/MainActivity.java
+++ UsingRecyclerView/app/src/main/java/k63/quocbao/usingrecyclerview/MainActivity.java
@@ -7,18 +7,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recycleViewDatas;
+    RecyclerView recyclerViewlandscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //3
+        recycleViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewlandscape = findViewById(R.id.recycleLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear =new LinearLayoutManager(this);
+        recyclerViewlandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this,recycleViewDatas);
+        //7
+        recyclerViewlandscape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("flag_tower_of_hanoi","Cot_co_Ha+noi");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("effel","Thap Effel"));
+        dsDuLieu.add(new LandScape("buckingham","Cung dien BuckingHam"));
+        dsDuLieu.add(new LandScape("statue_of_liberty","Tuong Nu Than Tu Do"));
+        return dsDuLieu;
+
+
     }
 }
